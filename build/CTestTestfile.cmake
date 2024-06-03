6fb51e4e13e1631b4f2fbd5113f9e760fd19ce03
maysa-bik <maysa.bik@laplateforme.com>
2024-06-03 10:48:45 +0200

    je telecharge le SFML

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/33753/OneDrive/Bureau/POODPUMLtest
+# Build directory: C:/Users/33753/OneDrive/Bureau/POODPUMLtest/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
